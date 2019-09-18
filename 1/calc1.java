4181149e17adecbd55c3b8e36de325dd52a74de1
Kaoru Ishizawa <kaoru.ishizawa@nnlife.co.jp>
2019-09-18 21:15:26 +0900

    9/18のソースコード

---
+++ 1/calc1.java
@@ -0,0 +1,5 @@
+public class calc1 {
+    public static void main(String[] arg) {
+        System.out.print(arg[0]+arg[1]);
+    }
+}
---
+++ 1/calc2.java
@@ -0,0 +1,5 @@
+public class calc2 {
+    public static void main(String[] args) {
+        System.out.print(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+    }
+}
